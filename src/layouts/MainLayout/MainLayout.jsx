9a492fc6b96c4270d4cf503a7d3d64d98514c1bd
javohircoder <jav.inamov9804@gmail.com>
2026-01-13 09:54:25 +0500

    Fix: Fixing props

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -3,8 +3,8 @@ import Sidebar from '../../components/Sidebar/Sidebar';
 import styles from './MainLayout.module.scss';
 import {Outlet} from "react-router-dom";
 
-function MainLayout({children}) {
-    const [isOpen, setIsOpen] = useState(false);
+function MainLayout({}) {
+    const [isOpen, _setIsOpen] = useState(false);
 
     return (
         <div className={styles.wrapper}>
